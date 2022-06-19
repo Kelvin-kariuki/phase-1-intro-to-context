5596d2ddfa38568a37d61743f3b4cbc6df076e94
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-19 17:14:54 +0300

    commit tree 7266b9d8e9e183a459bf6b51816238e2054fb58d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function createEmployeeRecord([firstName,familyName,title,payPerHour]) {
+  const employeeData ={
+    firstName: firstName,
+    familyName: familyName,
+    payPerHour: payPerHour,
+    title: title,
+    timeInEvents: [],
+    timeOutEvents: []
+  }  
+  return employeeData
+}
+function createEmployeeRecords(params) {
+    // code enters here
+}
+function createTimeInEvents(params) {
+    // code enters here
+}
+function createTimeOutEvents(params) {
+    // code enters here
+}
+function hoursWorkedOnDate(params) {
+    // code enters here
+}
+function wagesEarnedOnDate(params) {
+    // Code enters here
+}
+function allWagesFor(params) {
+    // code enters here
+}
+function calculatePayroll(params) {
+    // code enters here
+}
